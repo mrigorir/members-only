30276c1fca6337f5965142c390a9fc023cf3c379
pountzas <nikos@pountzas.gr>
2021-05-19 19:05:03 +0300

    fix linter error

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   has_rich_text :body
   validates :title, presence: { message: 'is required' },
                     length: { in: 5..32, message: 'should be from 5 to 32 characters long' }
-  validates :body, presence: { message: 'is required' }, length: { minimum: 10, message: 'at least 10' }  
+  validates :body, presence: { message: 'is required' }, length: { minimum: 10, message: 'at least 10' }
   belongs_to :user
 end
